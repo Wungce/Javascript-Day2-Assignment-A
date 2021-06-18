c2c861e9da59924c2b12ec8cf5fc46ecb1a38f08
wangsh <wugcelama19@gmail.com>
2021-06-18 16:50:16 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const My_Info = {
+    "name":"Wangsh",
+    "class":"Dcom",
+    "subject":"Web Programming",
+    "rollno":12
+    
+    
+}
+
+
+console.log(My_Info);
+
+delete My_Info.rollno;
+
+console.log(My_Info);
